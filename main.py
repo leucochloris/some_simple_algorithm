cdbe56a127f3b766d317cf07d7290053f6219b9f
Alexandr Shesterikov <108606736+leucochloris@users.noreply.github.com>
2022-07-19 18:25:29 +0300

    Almost ready

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ for i in range(1, people, 1):
     a.append(i)
 
 # generate a lot of empty massif`s
-for i in range(0, int(people/count_team), 1):
+for i in range(0, int(people / count_team), 1):
     n.append([])
+# print(a, '\n', n)
+
+
+while len(n[0]) !=5:
+    n[0].append((random.choice(a)))
+
 print(n)
 
 # random index of all members
 # print("Сотрудник", a[random.choice(a)])
-
-# all members of team
-# print(a)
