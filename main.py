411c7c77cc38835042b6ef339b12e41850ab84d3
Alexandr Shesterikov <108606736+leucochloris@users.noreply.github.com>
2022-07-19 16:27:00 +0300

    we have developed the logic of filling the array

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import random
 
-a = []
-b = []
+people = 100
+count_team = 20
 
-#generator of all members
-for i in range(1, 101, 1):
+a = []  # all members
+n = []  # here should be massif`s
+
+# generator of all members
+for i in range(1, people, 1):
     a.append(i)
 
+# generate a lot of empty massif`s
+for i in range(0, int(people/count_team), 1):
+    n.append([])
+print(n)
+
 # random index of all members
-print(a[random.choice(a)])
+# print("Сотрудник", a[random.choice(a)])
 
 # all members of team
-print(a)
+# print(a)
