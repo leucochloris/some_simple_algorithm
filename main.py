80fc0d8bdeca24ab4e958c711ef651931bbf067c
Alexandr Shesterikov <108606736+leucochloris@users.noreply.github.com>
2022-07-19 20:16:16 +0300

    Is there a lot of bags but just need to do a backup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
 people = 100
-count_team = 20
+members_inside_team = 5
 
 a = []  # all members
 n = []  # here should be massif`s
@@ -11,15 +11,26 @@ for i in range(1, people, 1):
     a.append(i)
 
 # generate a lot of empty massif`s
-for i in range(0, int(people / count_team), 1):
+for i in range(0, int(people / members_inside_team), 1):
     n.append([])
 # print(a, '\n', n)
 
 
-while len(n[0]) !=5:
+while len(n[0]) != 5:
     n[0].append((random.choice(a)))
 
-print(n)
+    while len(n[1]) != 5:
+        n[1].append((random.choice(a)))
 
+        while len(n[2]) != 5:
+            n[2].append((random.choice(a)))
+
+            while len(n[3]) != 5:
+                n[3].append((random.choice(a)))
+
+                while len(n[4]) != 5:
+                    n[4].append((random.choice(a)))
+
+print(n)
 # random index of all members
 # print("Сотрудник", a[random.choice(a)])
