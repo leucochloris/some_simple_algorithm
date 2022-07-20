e57bf9dbbc0a086ad7a7d744cb2263cfd70c8d45
Alexandr Shesterikov <108606736+leucochloris@users.noreply.github.com>
2022-07-20 10:12:35 +0300

    Fix bags

--- main.py
+++ main.py
@@ -1,36 +1,30 @@
 import random
 
-people = 100
+people = 101
 members_inside_team = 5
 
 a = []  # all members
 n = []  # here should be massif`s
 
 # generator of all members
-for i in range(1, people, 1):
+for i in range(1, people):
     a.append(i)
 
 # generate a lot of empty massif`s
-for i in range(0, int(people / members_inside_team), 1):
+for i in range(0, int(people / members_inside_team)):
     n.append([])
 # print(a, '\n', n)
 
 
-while len(n[0]) != 5:
-    n[0].append((random.choice(a)))
+# while len(n[:]) != 5:
+#     n[:].append((random.choice(a)))
 
-    while len(n[1]) != 5:
-        n[1].append((random.choice(a)))
 
-        while len(n[2]) != 5:
-            n[2].append((random.choice(a)))
+for x, el_2 in enumerate(n):
+    for j in range(members_inside_team):
+        vrem = random.choice(a)
+        el_2.append(vrem)
+        a.remove(vrem)
 
-            while len(n[3]) != 5:
-                n[3].append((random.choice(a)))
-
-                while len(n[4]) != 5:
-                    n[4].append((random.choice(a)))
-
-print(n)
-# random index of all members
-# print("Сотрудник", a[random.choice(a)])
+for eksl in n:
+    print(eksl)
