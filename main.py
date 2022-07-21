4f10d6d3c2039ef3642a6330ce04c2ec09c313fd
Alexandr Shesterikov <108606736+leucochloris@users.noreply.github.com>
2022-07-21 19:58:44 +0300

    just fixed some syntax errors

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ n = []  # here should be massif`s
 for i in range(1, people):
     a.append(i)
 
-# generate a lot of empty massif`s
+# generate a lot of empty arrays
 for i in range(0, int(people / members_inside_team)):
     n.append([])
 # print(a, '\n', n)
